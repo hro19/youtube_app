15f4c5fe5f72bd5c5a4a12c626d0c956b6833e5f
hro19 <fourbasic19@gmail.com>
2023-12-16 14:38:07 +0900

    タイトル文字を調整

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,7 +7,7 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1 class="bg-red-700 text-3xl">{{ msg }}</h1>
+  <h1 class="bg-red-700 text-2xl py-2">{{ msg }}</h1>
 
   <div class="card">
     <button class="" type="button" @click="count++">count is {{ count }}</button>
