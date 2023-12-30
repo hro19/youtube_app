e2a59da87648fde6445bf9efb0faed49454dc649
hro19 <fourbasic19@gmail.com>
2023-12-30 21:15:55 +0900

    computedを使って、日付データを整形する

--- src/components/SearchBoxItem.vue
+++ src/components/SearchBoxItem.vue
@@ -1,23 +1,35 @@
 <template>
   <li>
-    <img :src="item.snippet.thumbnails.default.url">
-    
+    <img :src="item.snippet.thumbnails.default.url" />
+
     <div>
-        <p><a class="underline" target="_blank" :href="'https://www.youtube.com/watch?v=' + item.id.videoId">{{ item.snippet.title }}</a></p>
-        <p>投稿日：{{ dateUntilDayJap(item.snippet.publishedAt) }}</p>
-        <p>チャンネル名:{{ item.snippet.channelTitle }}</p>
-        <p>チャンネルID:{{ item.snippet.channelId }}</p>
+      <p>
+        <a
+          class="underline"
+          target="_blank"
+          :href="'https://www.youtube.com/watch?v=' + item.id.videoId"
+          >{{ item.snippet.title }}</a
+        >
+      </p>
+      <p>投稿日：{{ dateJap }}</p>
+      <p>チャンネル名:{{ item.snippet.channelTitle }}</p>
+      <p>チャンネルID:{{ item.snippet.channelId }}</p>
     </div>
   </li>
 </template>
 
 <script setup lang="ts">
-import { dateUntilDayJap } from '../lib/dateFns'; 
+import { computed } from "vue";
+import { dateUntilDayJap } from "../lib/dateFns";
 
-defineProps({
-  item: { 
+const props = defineProps({
+  item: {
     type: Object,
-    required: true
-  }  
-})
+    required: true,
+  },
+});
+
+const dateJap = computed(() => {
+  return dateUntilDayJap(props.item.snippet.publishedAt);
+});
 </script>
