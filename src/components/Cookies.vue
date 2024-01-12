20a2e7262a2aa1cf8bd96cbe614114873037dcff
hro19 <fourbasic19@gmail.com>
2024-01-12 21:16:14 +0900

    cookieにya_usernameの情報を持っていないならば入力用のポップアップを表示

--- src/components/Cookies.vue
+++ src/components/Cookies.vue
@@ -1,18 +1,18 @@
 <script lang="ts" setup>
 import { useCookies } from '@vueuse/integrations/useCookies'
 
-const cookies = useCookies(['username'])
+const cookies = useCookies(['ya_username'])
 </script>
 
 <template>
   <div class="text-white">
-    <strong>ユーザーネーム</strong>: {{ cookies.get('username') }}
+    <strong>ユーザーネーム</strong>: {{ cookies.get('ya_username') }}
     <hr>
-    <button @click="cookies.set('username', 'ダミアン',{expires: new Date(Date.now() + (24 * 60 * 60 * 1000)),})">
+    <button @click="cookies.set('ya_username', 'ダミアン',{expires: new Date(Date.now() + (24 * 60 * 60 * 1000)),})">
       ダミアン
     </button>
     <br />
-    <button @click="cookies.set('username', 'ルーカス',{expires: new Date(Date.now() + (24 * 60 * 60 * 1000)),})">
+    <button @click="cookies.set('ya_username', 'ルーカス',{expires: new Date(Date.now() + (24 * 60 * 60 * 1000)),})">
       ルーカス
     </button>
   </div>
--- src/components/UserName.vue
+++ src/components/UserName.vue
@@ -1,29 +1,40 @@
 <template>
-  <div>
-    <!-- <button type="button" class="btn btn-primary" @click="showModal = true">Open modal</button> -->
-    <Modal v-model="showModal" title="ユーザーネーム" wrapper-class="modal-wrapper">
-      <div class="mb-3">
-        <el-input placeholder="ユーザーネームを入力ください" v-model="input"></el-input>
-      </div>
-      <div class="row">
-        <div class="col-sm-12">
-          <div class="flex justify-end gap-2">
-            <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full" type="button">ログイン</button>
-            <button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full" type="button" @click="showModal = false">Close</button>
-          </div>
-        </div>
-      </div>
-    </Modal>
-  </div>
+    <div>
+        <!-- <button type="button" class="btn btn-primary" @click="showModal = true">Open modal</button> -->
+        <Modal v-model="showModal" title="ユーザーネーム" wrapper-class="modal-wrapper">
+            <div class="mb-3">
+                <el-input placeholder="ユーザーネームを入力ください" v-model="input"></el-input>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="flex justify-end gap-2">
+                        <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full" type="button">ログイン</button>
+                        <button class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full" type="button" @click="showModal = false">Close</button>
+                    </div>
+                </div>
+            </div>
+        </Modal>
+    </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, onBeforeMount } from "vue";
 import { Modal } from "@kouts/vue-modal";
 import '@kouts/vue-modal/dist/vue-modal.css'
+import { useCookies } from '@vueuse/integrations/useCookies'
 
+const cookies = useCookies(['ya_username'])
 const input = ref("");
-const showModal = ref(true);
+const showModal = ref(false);
+
+onBeforeMount(() => {
+    if (!cookies.get('ya_username')) {
+        showModal.value = true;
+    }
+});
+
+
+
 </script>
 
 <style>
