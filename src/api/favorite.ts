f0e2a68bb8fdcb6694199287fb1a33c007008534
hro19 <fourbasic19@gmail.com>
2024-01-15 11:19:10 +0900

    videoIdをもとにお気に入りを削除する関数作成

--- src/api/favorite.ts
+++ src/api/favorite.ts
@@ -7,3 +7,12 @@ export const favoriteAll = async (username: string): Promise<any> => {
     // console.log(responseJson);
     return responseJson;
 }
+
+export const favoriteDel = async (username: string, videoId: string): Promise<any> => {
+    const url = `https://kvfirst.firstfirst.workers.dev/api/videos/${username}/del/${videoId}`;
+    const response = await fetch(url, {
+        method: 'PUT'
+    });
+    const responseJson = await response.json();
+    return responseJson;
+}
--- src/components/Favorites.vue
+++ src/components/Favorites.vue
@@ -4,9 +4,9 @@
     <ul>
         <li v-for="item in favorites.videos" :key="item.id" class="flex justify-between mb-3">
             <a :href="'https://www.youtube.com/watch?v=' + item.videoId" class="flex justify-between gap-2" target="_blank">
-                <img :src="item.thumbnail" width="90" />{{ item.title.slice(0, 20) }}{{ item.title.length > 20 ? '....' : '' }}
+                <img :src="item.thumbnail" width="50" />{{ item.title.slice(0, 20) }}{{ item.title.length > 20 ? '....' : '' }}
             </a>
-            <el-button type="success" plain @click="" class="ml-2">解除</el-button>
+            <el-button type="success" plain @click="favoriteDelFunc(cookiesStore.getUsername(),item.videoId)" class="ml-2">解除</el-button>
         </li>
     </ul>
 </template>
@@ -32,9 +32,13 @@ const favorites = ref<any>([]);
 //     favorites.value = await dbFavorites.getsAll();
 //     }
 
-import { favoriteAll } from "../api/favorite"
+import { favoriteAll,favoriteDel } from "../api/favorite"
 onMounted(async () => {
     favorites.value = await favoriteAll(cookiesStore.getUsername());
 });
 
+const favoriteDelFunc = async (username: string,videoId:string) => {
+    await favoriteDel(username,videoId);
+    favorites.value = await favoriteAll(username);
+}
 </script>
